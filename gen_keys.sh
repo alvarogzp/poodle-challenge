0bea17e235e724021e92424b99c210dc33525e53
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2015-03-23 03:32:59 +0100

    Generated base64 encoded keys

--- gen_keys.sh
+++ gen_keys.sh
@@ -17,12 +17,18 @@ one_line()
 	head --lines=1
 }
 
+base64_encode()
+{
+	echo -n "$1" | base64
+}
+
 echo "{"
 for i in $(seq ${1:-10})
 do
 	csrf="$(get_random_data 10 | one_line)"
-	user="$(get_random_data 5 | starting_with "[a-z]" | one_line)"
+	user="$(get_random_data 4 | starting_with "[a-z]" | one_line)"
 	password="$(get_random_data 10 | one_line)"
-	echo "\t\"$csrf\": \"$user:$password\","
+	encoded_user_password="$(base64_encode "$user:$password")"
+	echo "\t\"$csrf\": \"$encoded_user_password\","
 done
 echo "}"
