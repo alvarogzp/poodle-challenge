1a65989924ca5682e4687ba301eb4a0302e13033
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2015-03-23 22:42:15 +0100

    Updated gen to generate also input and output, and created directory structure

--- gen_inputs_outputs_tokens.sh
+++ gen_inputs_outputs_tokens.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+TOKENS_FILE="daemon/tokens"
+SUBMIT_PATH="submit"
+TEST_PATH="test"
+
 get_random_data()
 {
 	local wide="$1"
@@ -22,13 +26,19 @@ base64_encode()
 	echo -n "$1" | base64
 }
 
-echo "{"
-for i in $(seq ${1:-10})
+echo "{" > "$TOKENS_FILE"
+for i in $(seq 0 $(( ${1:-10} - 1 )))
 do
 	csrf="$(get_random_data 10 | one_line)"
+	echo "$csrf" > "$TEST_PATH/input$i"
+	echo "$csrf" > "$SUBMIT_PATH/input$i"
+	
 	user="$(get_random_data 4 | starting_with "[a-z]" | one_line)"
 	password="$(get_random_data 10 | one_line)"
 	encoded_user_password="$(base64_encode "$user:$password")"
-	echo "\t\"$csrf\": \"$encoded_user_password\","
+	echo "$encoded_user_password" > "$TEST_PATH/output$i"
+	echo "$encoded_user_password" > "$SUBMIT_PATH/output$i"
+	
+	echo "\t\"$csrf\": \"$encoded_user_password\"," >> "$TOKENS_FILE"
 done
-echo "}"
+echo "}" >> "$TOKENS_FILE"
